0f3435c13cd25fcd6eaabcf17a6aae38d330f90f
SrgElephant <36934732+SrgElephant@users.noreply.github.com>
2021-10-13 21:22:45 -0500

    Update scaCredentials.py

--- scaCredentials.py
+++ scaCredentials.py
@@ -18,33 +18,18 @@ f0.close()
 credentialsStr += input("Schnucks Account email: ") + "\n"
 credentialsStr += input("Schnucks Account password: ") + "\n"
 
-# Credentials file
-f1 = open("cred.txt", "wb")
-f1.write(fernet.encrypt(credentialsStr.encode()))
-f1.close()
-
 # Decide on emails
 sendEmails = input("Setup email credentials to send emails (y)?: ")
 if sendEmails == "y" or sendEmails == "yes":
 	# Optional input
-	emailAddress         = input("Sender email address: ")
-	emailPassword        = input("Sender email password: ")
-	emailAddressReceiver = input("Receiver email address: ")
-	smtp_server          = input("SMTP server address\nReccomended: smtp.gmail.com: ")
-	port                 = input("SMTP server port\nReccomended: 465: ")
-	
-	# Encrypt
-	encEmailAddress         = fernet.encrypt(emailAddress.encode())
-	encEmailPassword        = fernet.encrypt(emailPassword.encode())
-	encEmailAddressReceiver = fernet.encrypt(emailAddressReceiver.encode())
-	
-	# Write
-	f.write(str(encEmailAddress) + "\n")
-	f.write(str(encEmailPassword) + "\n")
-	f.write(str(encEmailAddressReceiver) + "\n")
-	f.write(str(smtp_server) + "\n")
-	f.write(str(port) + "\n")
+	credentialsStr += input("Sender email address: ") + "\n"
+	credentialsStr += input("Sender email password: ") + "\n"
+	credentialsStr += input("Receiver email address: ") + "\n"
+	credentialsStr += input("SMTP server address\nReccomended: smtp.gmail.com: ") + "\n"
+	credentialsStr += input("SMTP server port\nReccomended: 465: ") + "\n"
 
-print("Credentials complete.")
-f0.close()
+# Credentials file
+f1 = open("cred.txt", "wb")
+f1.write(fernet.encrypt(credentialsStr.encode()))
 f1.close()
+print("Credentials complete.")
